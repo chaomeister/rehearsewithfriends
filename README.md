5b3a74535649791ba9d44f33db7a09d89c55668e
Jonathan Chao <JChao@nbp-150-183.nbp.ruw.rutgers.edu>
2017-01-21 15:10:57 -0500

    add person functionality

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-flask-boilerplate
-=================
-
-Ready to use and deploy to Heroku!
-
-[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+Personal Site
--- app.py
+++ app.py
@@ -1,22 +1,34 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
 import jinja2
 import os
 
 app = Flask(__name__)
-
-@app.route('/')
+dbclient = MongoClient("mongodb://chao:rehearsewithfriends1@ds049094.mlab.com:49094/rehearsewithfriendsdb")
+db = dbclient.get_default_database()
+people = db.People
+@app.route('/', methods=['GET'])
 def hello():
-	return render_template("index.html")
+	if request.args.get("error") is not None:
+		return render_template("index.html",people=people.find(),error=request.args.get("error"))
+	return render_template("index.html",people=people.find())
 
-@app.route('/change')
-def chance():
+@app.route('/add', methods=['POST'])
+def add():
+	name = request.form.get("name")
+	location = request.form.get("location")
+	stime = request.form.get("stime")
+	etime = request.form.get("etime")
+	if name == "" or location == "":
+		return redirect(url_for('.hello', error="Please completely fill out the form"))
+	people.insert_one(
+		{
+		"name":name,
+		"location":location,
+		"stime":stime,
+		"etime":etime
+		})
 	return redirect('/')
-
-@app.route('/post', methods=['GET','POST'])
-def post():
-	if request.method == 'POST':
-		return render_template('post.html')
-	return render_template('get.html')
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 8000))

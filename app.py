8cbd1fc13e6366af9650b87960d4d745aef05fde
Jonathan Chao <JChao@nbp-174-150.nbp.ruw.rutgers.edu>
2017-01-26 22:13:15 -0500

    added delete and fix style

--- app.py
+++ app.py
@@ -2,16 +2,28 @@ from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 import jinja2
 import os
+import datetime
 
 app = Flask(__name__)
 dbclient = MongoClient("mongodb://chao:rehearsewithfriends1@ds049094.mlab.com:49094/rehearsewithfriendsdb")
 db = dbclient.get_default_database()
 people = db.People
+
+
 @app.route('/', methods=['GET'])
 def hello():
+	deleteFinishedRehearsals()
 	if request.args.get("error") is not None:
 		return render_template("index.html",people=people.find(),error=request.args.get("error"))
 	return render_template("index.html",people=people.find())
+
+def deleteFinishedRehearsals():
+	allPeople = people.find()
+	curr_time = datetime.datetime.now().time()
+	for person in allPeople:
+		end_time = datetime.datetime.strptime(person["etime"], "%H:%M").time()
+		if curr_time > end_time:
+			people.delete_one({'_id':person['_id']})
 
 @app.route('/add', methods=['POST'])
 def add():
